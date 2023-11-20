4a0bfc3abfcd0d1ad41d05a17e5f3b79b799cb7c
Benjamin Peters <bpeters@mpi-sws.org>
2023-11-20 10:47:28 +0100

    did vim stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -16,7 +16,12 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 
 require("lazy").setup({
-	{ import = "plugins" },
+	{ 
+        import = "plugins",
+        defaults = {
+            lazy = false
+        },
+    },
 
 	-- { "Valloric/YouCompleteMe", build="./install.py" },
 },
--- nvim/.config/nvim/lua/plugins/latex.lua
+++ nvim/.config/nvim/lua/plugins/latex.lua
@@ -2,10 +2,25 @@ return {
 	{
 		"lervag/vimtex",
 		config = function()
-			vim.g.vimtex_view_method = "zathura"
+            if vim.fn.has("macunix") then
+                vim.g.vimtex_view_method = "skim"
+            else
+                vim.g.vimtex_view_method = "zathura"
+            end
 			vim.g.vimtex_mappings_prefix = "<leader>m"
             vim.g.vimtex_compiler_latexmk_engines = { _ = "-lualatex" }
             vim.g.vimtex_quickfix_ignore_filters = { "Marginpar on page" };
+
+            vim.api.nvim_create_autocmd("BufRead", {
+                pattern = {
+                    "/Users/silvus/phd/semantics/*",
+                    "/Users/silvus/phd/vorkurs/*",
+                },
+                callback = function()
+                    vim.g.vimtex_compiler_latexmk_engines = { _ = "-pdf" }
+                end,
+            })
+
 		end,
 	},
 }
